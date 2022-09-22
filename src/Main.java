261b2b72057e6c8053d36fa996dcd6094de0f0ad
alu10722411 <alutoa2411@ieselcaminas.org>
2022-09-22 16:49:38 +0200

    second commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Exercise10{
+    public static void main(String[] args){
+         int x;
+        System.out.println("Enter a number:");
+
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        x = inputValue.nextInt();
+
+        if (x >= 0){
+            System.out.println("The number is positive");;
+        } else {
+            System.out.println("The number is negative");
+        }
+    }
+}
